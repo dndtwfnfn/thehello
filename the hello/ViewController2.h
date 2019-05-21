daff502ce4719fb445fe7ad5ff7ec29656eebef1
臧传志 <zczwzf@126.com>
2019-05-21 19:28:01 +0800

    1

---
+++ the hello/ViewController2.h
@@ -0,0 +1,29 @@
+//
+//  ViewController2.h
+//  the hello
+//
+//  Created by 臧 传志 on 2018/11/7.
+//  Copyright © 2018年 臧 传志. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController2 : UIViewController{
+
+UILabel *zlabel;
+UILabel *zlabelstyle;
+
+UILabel *labela;
+UILabel *labelb;
+UILabel *labelc;
+UILabel *labeld;
+UILabel *labele;
+UILabel *labelf;
+UILabel *labelok;
+UILabel *labelcount;
+    UICollectionView *zti;
+}
+@property (nonatomic,strong) UISwipeGestureRecognizer *leftSwipeGestureRecognizer;
+@property (nonatomic,strong) UISwipeGestureRecognizer *rightSwipeGestureRecognizer;
+
+@end
